70756d5f88dedfbe12cffe81551f1ee635c016bb
C4LV1NPU6 <89880125+C4LV1NPU6@users.noreply.github.com>
2022-09-30 22:17:35 -0600

    Add files via upload

---
+++ Family Map Android App/FamilyMapClient/app/build/generated/source/buildConfig/debug/com/example/familymapclient/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.familymapclient;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.familymapclient";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String MAPS_API_KEY = "AIzaSyCyJUsHXziYCF_6NLsm9fWp8MaMf6gzl2I";
+}
